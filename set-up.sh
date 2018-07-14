8e6eca58ac4dc57e7946143fb7f125400db2bea0
tamago0224 <bigtreesasuke200733@gmail.com>
2018-07-14 11:52:05 +0900

    'update'

--- set-up.sh
+++ set-up.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
-mkdir -p ~/.vim/dein/repos/github.com/Shougo/dein.vim
+SCRIPT_DIR=$(cd ($(dirname $0); pwd)
+
+cd $HOME/src/vim
+./configure --with-features=huge \
+--enable-fail-if-missing \
+--enable-setfont \
+--enable-terminal \
+--enable-gtk2-check \
+--enable-gnome-check \
+--enable-pythoninterp=dynamic \
+--enable-python3interp=dynamic \
+--enable-luainterp=dynamic \
+--enable-rubyinterp=dynamic
+
+make && make install
+
+mkdir -p $HOME/.vim/dein/repos/github.com/Shougo/dein.vim
 git clone https://github.com/Shougo/dein.vim.git  ~/.vim/dein/repos/github.com/Shougo/dein.vim
 
+cd $HOME
+ln -s $SCRIPT_DIR/.vimrc $HOME/.vimrc
