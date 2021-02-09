953e7d2ce798290698d74a2b57dfb00102aa8fbe
tamago0224 <bigtreesasuke200733@gmail.com>
2021-02-09 09:27:35 +0900

    fix vim-plug install script

--- install-vim-plug.sh
+++ install-vim-plug.sh
@@ -2,5 +2,10 @@
 
 # install vim plugin manager vim-plug
 
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+if [[ ! -d ${HOME}/.vim/autoload/plug.vim ]]; then
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+else
+    echo "already installed vim-plug."
+    exit 1
+fi
