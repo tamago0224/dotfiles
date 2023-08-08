5e0e35353e5f52de1d611370902f0cbb3df88222
tamago0224 <bigtreesasuke200733@gmail.com>
2023-08-09 07:06:59 +0900

    update

---
+++ .config/nvim/lua/tamago/plugins/lsp/mason.lua
@@ -0,0 +1,25 @@
+local mason_status, mason = pcall(require, "mason")
+if not mason_status then
+  return
+end
+
+local mason_lspconfig_status, mason_lspconfig = pcall(require, "mason-lspconfig")
+if not mason_lspconfig_status then
+  return
+end
+
+mason.setup()
+
+mason_lspconfig.setup({
+  ensure_installed = {
+    "tsserver",
+    "html",
+    "cssls",
+    "dockerls",
+    "docker_compose_language_service",
+    "gopls",
+    "java_language_server",
+    "lua_ls",
+    "pylsp",
+  }
+})
