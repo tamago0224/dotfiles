ce16444fc0f16a7469bd422c6be27c09b25fa5aa
tamago0224 <bigtreesasuke200733@gmail.com>
2024-10-30 10:23:44 +0900

    update

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,14 +1,9 @@
 local wezterm = require("wezterm")
 
 local config = {}
-if wezterm.config_builder then
-	config = wezterm.config_builder()
-end
-
-config.default_prog = { "wsl.exe", "--distribution", "MyUbuntu", "--cd", "~" }
 
 config.warn_about_missing_glyphs = false
-config.font = wezterm.font("DejaVuSansM Nerd Font")
+config.font = wezterm.font("UDEV Gothic 35NF")
 config.use_ime = true
 config.font_size = 14.0
 config.color_scheme = "Later This Evening"
@@ -24,7 +19,8 @@ config.window_background_gradient = {
 	colors = { "#000000" },
 }
 config.show_new_tab_button_in_tab_bar = false
-config.show_close_tab_button_in_tabs = true
+-- config.show_close_tab_button_in_tabs = true
+-- config.window_background_image = "/home/k-ohki/Pictures/barry_kun.png"
 config.hide_tab_bar_if_only_one_tab = true
 config.adjust_window_size_when_changing_font_size = false
 config.enable_wayland = false
@@ -141,6 +137,38 @@ wezterm.on("format-tab-title", function(tab, tabs, panes, _config, hover, max_wi
 		{ Foreground = { Color = edge_foreground } },
 		{ Text = SOLID_RIGHT_ARROW },
 	}
+end)
+
+-- ssh host background
+local host_to_color = {
+	["*.thop.2iij.net"] = "#FF0000",
+	-- key: pattern to match hostname,
+	-- value: color to use as pane background for the hostname
+}
+local colored_panes = {}
+wezterm.on("update-status", function(window, pane)
+	local fg = pane:get_foreground_process_info() or {}
+	if fg.name == "ssh" then
+		for ndx = 2, #fg.argv do
+			for k, v in pairs(host_to_color) do
+				wezterm.log_info("hogehoge", k, v)
+				if string.find(fg.argv[ndx], k) then
+					if not colored_panes[pane:pane_id()] then
+						pane:inject_output(string.format("\x1b]11;%s\x07", string.sub(v, 2)))
+						colored_panes[pane:pane_id()] = fg.pid
+					end
+				end
+			end
+		end
+	else
+		if colored_panes[pane:pane_id()] then
+			local ssh = wezterm.procinfo.get_info_for_pid(colored_panes[pane:pane_id()])
+			if not ssh then
+				colored_panes[pane:pane_id()] = nil
+				pane:inject_output("\x1b]104\x07")
+			end
+		end
+	end
 end)
 
 return config
